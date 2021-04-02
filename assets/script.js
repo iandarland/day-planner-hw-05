e2c25e7f81285dfeeb5d0ed072fda8dd671bd651
Ian Darland <iansmbpro@ians-mbp.lan>
2021-04-01 22:26:33 -0500

    adjusting so all items refersh daily

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 //Setting universal variables
 var body = $('.container');
 var times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-var pulledTodos = JSON.parse(localStorage.getItem("saved"))|| []
+var pulledTodos = JSON.parse(localStorage.getItem(saved))|| []
+var saved = JSON.stringify(moment().format("MMM Do YY"));  
 
 //Setting function update clock
 var update = function() {
@@ -18,14 +19,14 @@ $('.custom-submit').on('click', function(){
     var todo = $(this).siblings('.custom-input').val();
     var timeStamp = $(this).siblings('.hour-box').text().trim();
     var savedTodos = {timeStamp, todo}
-    var pulledTodos = JSON.parse(localStorage.getItem("saved"))|| [];
+    var pulledTodos = JSON.parse(localStorage.getItem(saved))|| [];
     pulledTodos.push(savedTodos)
-    localStorage.setItem("saved", JSON.stringify(pulledTodos))
+    localStorage.setItem(saved, JSON.stringify(pulledTodos))
 });
 
 // setting styling by hour and disabling button and input boxes for times in the past
 $(".hour-box").each(function(index){
-    if(parseInt(moment($(this).text().trim(), ["h:mm A"]).format("H")) < moment().format('H')){
+    if(parseInt(moment($(this).text().trim(), ["h:mm A"]).format("H")) > moment().format('H')){
         $(this).css('background', "#F24E29").siblings('button').css('background', "#F24E29").prop('disabled', 'true').siblings("input").prop('disabled', 'true')
     }else if (moment($(this).text().trim(), ["h:mm A"]).format("H") === moment().format('H')){
         $(this).css('background', '#F2B9B3').siblings('button').css('background', '#F2B9B3')
