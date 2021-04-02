4b6fa4aa0f74111bb819eb2dda7a1ac01943a6d8
Ian Darland <iansmbpro@ians-mbp.lan>
2021-04-01 22:10:54 -0500

    final touches and readme update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # day-planner-hw-05
+
+## Description
+This is a simple daily schedule planner. Use the input boxes to plan your day, but make sure to save when your plan is set in stone! If your plan is in the hour for the hour, the schedule will alert you by changing colors to a soft pink. If your plans are in the past the schedule will let you know by changing the color to red. Boxes in the past are no longer accessable. An updated time clock is available in at the top of the screen at all times for your reference. 
+
+![screenshot of working site](assets/images/Work Day Schedul.png)
--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
+//Setting universal variables
 var body = $('.container');
 var times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 var pulledTodos = JSON.parse(localStorage.getItem("saved"))|| []
+
+//Setting function update clock
 var update = function() {
     document.getElementById("currentDay")
     .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
+
+//Header clock feature and calling update function
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 setInterval(update, 1000);
 
-$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+//Saving values of input box to local storage on click based on location pushing them to pulledTodos array
 $('.custom-submit').on('click', function(){
     var todo = $(this).siblings('.custom-input').val();
     var timeStamp = $(this).siblings('.hour-box').text().trim();
@@ -17,6 +23,7 @@ $('.custom-submit').on('click', function(){
     localStorage.setItem("saved", JSON.stringify(pulledTodos))
 });
 
+// setting styling by hour and disabling button and input boxes for times in the past
 $(".hour-box").each(function(index){
     if(parseInt(moment($(this).text().trim(), ["h:mm A"]).format("H")) < moment().format('H')){
         $(this).css('background', "#F24E29").siblings('button').css('background', "#F24E29").prop('disabled', 'true').siblings("input").prop('disabled', 'true')
@@ -24,9 +31,8 @@ $(".hour-box").each(function(index){
         $(this).css('background', '#F2B9B3').siblings('button').css('background', '#F2B9B3')
     }
 });
-var save= function(){
-    $(this).siblings('input').text(winner);
-};
+
+//Populating saved values in the dom
 $('.hour-box').each(function(index){
     var toGet = JSON.stringify($(this).text().trim())
     for (i = 0; i < pulledTodos.length; i++){
